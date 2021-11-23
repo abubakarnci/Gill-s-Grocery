26fad1c522b2728dfab24872a63c92b50af5d1ca
EC2 Default User <ec2-user@ip-172-31-18-79.ec2.internal>
2021-11-23 20:58:01 +0000

    AWS RDS

--- grocery/grocery/settings.py
+++ grocery/grocery/settings.py
@@ -77,6 +77,18 @@ WSGI_APPLICATION = 'grocery.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'cpp_project',
+#            #os.path.join(BASE_DIR, 'db.sqlite3'),
+#        'USER' : 'abubakar',
+#        'PASSWORD': '123456789',
+#        'HOST' :'database-2.c7mf0ysgbowi.us-east-1.rds.amazonaws.com',
+#        'PORT': '5432'
+#    }
+#}
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
